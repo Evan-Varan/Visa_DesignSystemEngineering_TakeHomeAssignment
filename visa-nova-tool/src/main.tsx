48a9bdbf887635c9d243e1c58dd192f91471ea1c
Evan Varan <evan.varan@gmail.com>
2025-07-26 11:52:32 -0500

    Moving styles for deployment fix

--- visa-nova-tool/src/main.tsx
+++ visa-nova-tool/src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './nova-styles.css' // Import Visa Nova styles
-import "@visa/nova-styles/base/core.css";
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './nova-styles.css'; // This now includes your Visa styles
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
