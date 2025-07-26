43dc8ae90edbdc97ebd3188bafe004e7a9256bec
Evan Varan <evan.varan@gmail.com>
2025-07-26 11:42:25 -0500

    Test fix for Redeploy with imports

--- visa-nova-tool/src/main.tsx
+++ visa-nova-tool/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './nova-styles.css' // Import Visa Nova styles
+import "@visa/nova-styles/base/core.css";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
